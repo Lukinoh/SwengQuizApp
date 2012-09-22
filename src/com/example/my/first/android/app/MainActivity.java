bcb1577692f49af291383f0b51005177ba7c6e58
La Spada Luca <luca.laspada@epfl.ch>
2012-09-22 16:34:44 +0200

    End of the tutoriel from sweng

--- src/com/example/my/first/android/app/MainActivity.java
+++ src/com/example/my/first/android/app/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.my.first.android.app;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -16,5 +20,15 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    public void displayMessage(View view)
+    {
+    	Toast.makeText(this, "You click Display", Toast.LENGTH_SHORT).show();
+    	Intent displayActivityIntent = new Intent(this, ShowMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_text);
+    	String userText = editText.getText().toString();
+    	displayActivityIntent.putExtra(this.getClass().getName(), userText);
+    	startActivity(displayActivityIntent);
     }
 }
